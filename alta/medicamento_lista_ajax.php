fc28d708fa1227a2dbe3501bc67e976a085443a5
Javier1995 <quiroga218@gmail.com>
2019-07-10 21:15:34 -0500

    extraccion de información de los datos de medicamentos registrados

---
+++ alta/medicamento_lista_ajax.php
@@ -0,0 +1,28 @@
+<?php
+require_once '../autoload.php';
+use Medicamento\Medicamento;
+header("Content-type: application/json; charset=utf-8");
+//DATOS DE ENTRADA
+$search = $_POST['search'];
+$current_page =(!empty($_POST['page']) && $_POST['page'] > -1)? $_POST['page'] - 1 : 0;
+$limit = (!empty($_POST['limit']) && $_POST['limit'] >= 1)? $_POST['limit'] : 5;
+$offset = 0 + ($current_page * $limit);
+
+//LISTA DE MEDICAMENTO
+$med = new Medicamento();
+$rows = $med->getMedication($search);
+$medicamentos = $med->getMedication($search, $offset, $limit);
+$paginas = ceil($rows->num_rows/$limit);
+$adjacent = 2;
+$json_data = array();
+$header = array('Codigo','Nombre Comercial', 'Formula','Presentacion' ,'Precio Entrada', 'Precio salida', 'Categoria', 'stock Minimo', 'En inventario');
+$json_data = array('datos'=>array(),'pages'=>$paginas, 'current_page'=>$current_page, 'adjacent'=>$adjacent, 'limit'=>$limit, 'header'=>$header, 'search'=>$search);
+
+while($medicamento = $medicamentos->fetch_assoc()){
+    
+     array_push($json_data['datos'], $medicamento);
+
+}
+
+echo json_encode($json_data, JSON_INVALID_UTF8_SUBSTITUTE);
+
